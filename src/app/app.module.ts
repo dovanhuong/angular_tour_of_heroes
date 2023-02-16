3412df540c60c685a6f160b19b35b83e50a802a5
Tony Do <dvhbkhn@gmail.com>
2023-02-16 20:54:07 +0900

    updated learn componenet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CrisisListComponent } from './crisis-list/crisis-list.component';
-import { HeroListComponent } from './hero-list/hero-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CrisisListComponent,
-    HeroListComponent
   ],
   imports: [
     BrowserModule
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,21 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { Component, Input } from '@angular/core';
+@Component({
+  selector:'app-greeting',
+  template:'<h1>Hello, {{name}}!</h1>'
+})
+export class GreetingComponeent{
+  @Input() name:string;
+}
+
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+// platformBrowserDynamic().bootstrapModule(Component)
+//   .catch(err => console.error(err));

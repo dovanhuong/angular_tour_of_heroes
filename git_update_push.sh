c9c4e4c31d4fe9696767795a1e296573cd2c1e87
Tony Do <dvhbkhn@gmail.com>
2023-02-17 11:31:22 +0900

    checking

---
+++ git_update_push.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+git remote add origin https://github.com/dovanhuong/$1
+git branch -M main
+git pull
+git add .
+git commit -m "$2"
+git push -u origin main
+echo "Completed updated script"

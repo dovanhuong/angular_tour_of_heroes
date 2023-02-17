f9b282383d5c794ef820024459f33c84a3ebf765
Tony Do <dvhbkhn@gmail.com>
2023-02-17 11:28:22 +0900

    update component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './Container/container.component';
+import { NavComponent } from './nav/nav.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ContainerComponent,
+    NavComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule

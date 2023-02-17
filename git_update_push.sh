e97688cff87d1d3d3f05f4ed64a85ed32ecb9259
Tony Do <dvhbkhn@gmail.com>
2023-02-17 14:30:30 +0900

    edited git script

--- git_update_push.sh
+++ git_update_push.sh
@@ -3,10 +3,12 @@
 #Date: 17th Feb,2023
 #Email: vanhuong.do@asicland.com
 #
-git remote add origin https://github.com/dovanhuong/$1.git
+#git remote add origin https://github.com/dovanhuong/$1.git
+git remote add origin https://github.com/dovanhuong/angular_tour_of_heroes.git
 git branch -M main
 git pull
 git add .
-git commit -m "$2"
+#git commit -m "$2"
+git commit -m "$1"
 git push -u origin main
 echo "Completed updated script"
